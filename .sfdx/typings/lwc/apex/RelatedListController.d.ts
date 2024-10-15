2f7d5cb8ad3ffa4b0305219b51bdf6550dc73802
bhupendraSinghTanwar <bhupendra.singh@areya.tech>
2024-10-15 21:34:48 +0530

    Source code 3

---
+++ .sfdx/typings/lwc/apex/RelatedListController.d.ts
@@ -0,0 +1,9 @@
+declare module "@salesforce/apex/RelatedListController.initData" {
+  export default function initData(param: {jsonData: any}): Promise<any>;
+}
+declare module "@salesforce/apex/RelatedListController.getRecords" {
+  export default function getRecords(param: {jsonData: any}): Promise<any>;
+}
+declare module "@salesforce/apex/RelatedListController.getIconName" {
+  export default function getIconName(param: {sobjectApiName: any}): Promise<any>;
+}
